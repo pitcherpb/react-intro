179e7fb2998482dcd7150803284d9d4c41db2767
Pitcher <Pitcher@MacBook-Air-13.local>
2016-10-15 12:06:07 +0700

    React component

--- src/app.js
+++ src/app.js
@@ -1,13 +1,67 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const title = 'My React App'
+const SearchForm = () => {
+    return (
+        <form>
+            <input type="text" />
+            <button type="submit">search</button>
+        </form>
+    )
+}
+
+const Header = (props) => (
+    <hearder>
+        <h1>{props.title}</h1>
+        <SearchForm />
+    </hearder>
+)
+
+const Items = (props) => {
+    console.log(props.items)
+    return (
+        <ul>
+        {props.items.map(item => (
+            <li>{item}</li>
+        ))}
+        </ul>
+    )
+} 
+
+const Content = (props) => (
+    <section>
+        <p>{props.content}</p>
+        <Items items={props.items}/>
+    </section>
+)
+
+const AppWithoutDescription = () => (
+    <Header title="No description here" />
+)
 
 const App = () => {
+    const appTitle = 'Fronttechs: React'
+    const appContent = 'This is a simple react applicationt'
+    const items = [
+        "Oliver",
+        "Tobey",
+        "Charlie",
+        "Lucky",
+        "Poyo"
+    ]
+    return(
+        <section className="app-container">
+            <Header title={appTitle} />
+            <Content 
+                content={appContent}
+                items={items}
+            />
+        </section>
+    )
 
-    return <div> My React App </div>
 
 }
+
 
 const element = document.getElementById('app')
 ReactDOM.render(<App />, element)
